7ad5b0bb4a9511fbc109c0df12ba5babd320c3a6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-01-20 05:32:24 +0400

    Init commit

---
+++ lib/sentry_to_discord_for_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sentry_to_discord_for_rails/version"
+
+module SentryToDiscordForRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sentry_to_discord_for_rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SentryToDiscordForRails do
+  it "has a version number" do
+    expect(SentryToDiscordForRails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
